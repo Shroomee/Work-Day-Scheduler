0fbb53ec9977d721cbd3f4640598d5d0beb4e056
unknown <dudeomous@gmail.com>
2023-05-14 15:53:43 -0400

    fixed bugs in updateBlocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,37 @@
 //display current day in id currentDay
 var currentDay = dayjs().format("MMM D, YYYY");
-
 $(document).ready(function() {
   $("#currentDay").text(currentDay);
 });
 
 //update the time blocks
 function updateBlocks() {
-  var hour = dayjs().hour();
 
-  $('time-block').each(function() {
-    var hourBlock = parseInt($(this).attr('id'));
 
-    $(this).removeClass('past present future');
-
+  $('.time-block').each(function() {
+    var hourBlock = parseInt($(this).attr('id').split('-')[1]);
+    var hour = dayjs().hour();
     if (hourBlock < hour) {
       $(this).addClass('past')
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+
     } else if (hourBlock === hour) {
       $(this).addClass('present')
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+
     } else {
       $(this).addClass('future')
+      $(this).removeClass('past');
+      $(this).removeClass('present');
     }
   });
 }
 
 updateBlocks();
+
+
 //event handler for save button with local storage
 $('.saveBtn').on('click', function() {
   var text = $(this).siblings('.description').val();
